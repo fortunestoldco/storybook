0acf72aa72e5650fc652419626669cf991c7f1ae
David James Lennon <david@fortunestold.co>
2025-02-25 05:48:28 +0000

    Fix

--- oldsrc/src/storybook/config.py
+++
@@ -1,4 +0,0 @@
-class Config:
-    def __init__(self):
-        self.llm_router = "LLM Router Configuration"
-        self.mongo_db_connection = "MongoDB Connection String"
--- oldsrc/src/storybook/tools.py
+++
@@ -1,8 +0,0 @@
-from langchain_core.tools import BaseTool
-
-class ToolsService:
-    def __init__(self, llm_router):
-        self.llm_router = llm_router
-
-    def get_tool(self, tool_name):
-        return BaseTool(self.llm_router, tool_name)
